67e450a4554eb85425446c21d689e3e23b1b2cb3
Veronique Cadet <cadetvero@gmail.com>
2023-01-25 10:32:10 -0500

    dont merge

--- src/components/Matches.jsx
+++ src/components/Matches.jsx
@@ -1,29 +1,26 @@
-import React, {useState, useEffect} from 'react'
-import MatchCarousel from './MatchCarousel'
-import NavBar from './NavBar';
+import React, { useState, useEffect } from "react";
+import MatchCarousel from "./MatchCarousel";
+import NavBar from "./NavBar";
 
 function Matches() {
   const [matches, setMatches] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:4001/matches")
+    fetch("http://localhost:9292/users")
       .then((res) => res.json())
       .then((json) => {
-        setMatches(json)
-        console.log(matches)
+        setMatches(json);
+        console.log(matches);
       });
   }, []);
-      console.log(matches);
-
-
+  console.log(matches);
 
   return (
     <div>
-      <NavBar/>
-      <MatchCarousel matches = {matches}/>
+      <NavBar />
+      <MatchCarousel matches={matches} />
     </div>
-    
-  )
+  );
 }
 
-export default Matches
+export default Matches;
