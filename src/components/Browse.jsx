d56d40172783157a41a3340a3711d755f9eb517d
Olivia Radick <olivia.radick@gmail.com>
2023-01-25 18:19:12 -0500

    copy/pasted from main

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -6,6 +6,10 @@ function Browse() {
   const [potentialMatches, setPotentialMatches] = useState([])
   const [currentPotentialMatch, setCurrentPotentialMatch] = useState(0)
 
+  // function nextProfile(){
+  //   setCurrentPotentialMatch(currentPotentialMatch ++)
+  // }
+
   useEffect(() =>{
     fetch("http://localhost:9292/users")
     .then ((response) => response.json())
@@ -19,40 +23,71 @@ function Browse() {
   //handles the async request to avoid an "undefined" error
   if (!potentialMatches[currentPotentialMatch]) return null
 
+  function handleAccept(match){
+    fetch("http://localhost:9292/newmatch", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        likee_id: match.id,
+        liker_id: 1,
+      }),
+    }).then((r) => r.json());
+    // .then(setCurrentPotentialMatch(currentPotentialMatch+1));
+    setCurrentPotentialMatch(currentPotentialMatch+1)
+  }
+
+  function handleReject(){
+    setCurrentPotentialMatch(currentPotentialMatch + 1);
+  }
+
   return (
     <div>
       <Header />
-      <br/>
-      <div className='browse'>
+      <br />
+      <div className="browse">
         Browse Your Dating Pool
         <div>
-          <img className="prof-pic"src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png"/>
+          <img
+            className="prof-pic"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/1200px-Twemoji_1f600.svg.png"
+          />
           <div className="browse-container">
-            <div className='browse-details'>
+            <div className="browse-details">
               <h1>{potentialMatch.name}</h1>
               <h3>Gender: {potentialMatch.gender}</h3>
               <h3>Age: {potentialMatch.age}</h3>
               <h3>Location: {potentialMatch.location}</h3>
               <h3>Bio: {potentialMatch.bio}</h3>
               <h3>What are you looking for in a relationship?</h3>
-                <h4>{potentialMatch.prompt_1}</h4>
+              <h4>{potentialMatch.prompt_1}</h4>
               <h3>What are hobbies and passions?</h3>
-               <h4>{potentialMatch.prompt_2}</h4>
+              <h4>{potentialMatch.prompt_2}</h4>
               <h3>Two truths and a lie:</h3>
-               <h4>{potentialMatch.prompt_3}</h4>
+              <h4>{potentialMatch.prompt_3}</h4>
             </div>
-            <div className='browse-meme'>
-                    <img src={potentialMatch.meme}/>
-                  </div>
-                  </div>
-                  <div className='browse-buttons'>
-                    <img src='/assets/delete.png' alt='https://www.flaticon.com/free-icons/cross' onClick={()=>setCurrentPotentialMatch(currentPotentialMatch+1)}/>
-                    <img src='/assets/check.png' alt="https://www.flaticon.com/free-icons/foursquare-check-in" onClick={()=>setCurrentPotentialMatch(currentPotentialMatch+1)}/>
-                  </div>
+            <div className="browse-meme">
+              <img src={potentialMatch.meme} />
+            </div>
           </div>
+          <div className="browse-buttons">
+            <button onClick={handleReject}>
+              <img
+                src="/assets/delete.png"
+                alt="https://www.flaticon.com/free-icons/cross"
+                />
+            </button>
+            <img
+                onClick={()=>handleAccept(potentialMatch)}
+              src="/assets/check.png"
+              alt="https://www.flaticon.com/free-icons/foursquare-check-in"
+            />
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Browse
