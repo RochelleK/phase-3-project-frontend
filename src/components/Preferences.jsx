1e3aec06f8fd37b6d73c3388dc8cf32e758f198c
Veronique Cadet <cadetvero@gmail.com>
2023-01-26 17:17:47 -0500

    css

--- src/components/Preferences.jsx
+++ src/components/Preferences.jsx
@@ -1,13 +1,11 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 function Preferences() {
-
-const [genderPref, setGenderPref] = useState("All")
-const [locationPref, setLocationPref] = useState("NYC")
-const [politicalPref, setPoliticalPref] = useState("Liberal")
-const [smokerPref, setSmokerPref] = useState(false)
-const [drinkerPref, setDrinkerPref] = useState(false)
-
+  const [genderPref, setGenderPref] = useState("All");
+  const [locationPref, setLocationPref] = useState("NYC");
+  const [politicalPref, setPoliticalPref] = useState("Liberal");
+  const [smokerPref, setSmokerPref] = useState(false);
+  const [drinkerPref, setDrinkerPref] = useState(false);
 
   fetch("https://jsonplaceholder.typicode.com/posts/1", {
     method: "PATCH",
@@ -22,12 +20,16 @@ const [drinkerPref, setDrinkerPref] = useState(false)
     .then((json) => console.log(json));
 
   return (
-    <div id="form">
-      <form className="form">
+    <div className="profile-card-div">
+      <form>
         <p className="form-title">Preferences</p>
         <div>
           <label className="form-text">Looking for A </label>
-          <select name="gender" value={setGenderPref} onChange={(e)=> setGenderPref(e.target.value)}>
+          <select
+            name="gender"
+            value={setGenderPref}
+            onChange={(e) => setGenderPref(e.target.value)}
+          >
             <option value="Woman">Woman</option>
             <option value="Man">Man</option>
             <option value="Non-binary"> Non-binary</option>
@@ -37,7 +39,11 @@ const [drinkerPref, setDrinkerPref] = useState(false)
         </div>
         <div>
           <label className="form-text">Living In </label>
-          <select name="location" value={locationPref} onChange={(e)=> setLocationPref(e.target.value)} >
+          <select
+            name="location"
+            value={locationPref}
+            onChange={(e) => setLocationPref(e.target.value)}
+          >
             <option value="NYC">NYC</option>
             <option value="LA">LA</option>
             <option value="Miami">Miami</option>
@@ -46,28 +52,41 @@ const [drinkerPref, setDrinkerPref] = useState(false)
         </div>
         <div>
           <label className="form-text">Political View </label>
-          <select name="political" id="cars" value={politicalPref} onChange={(e)=> setPoliticalPref(e.target.value)}>
+          <select
+            name="political"
+            id="cars"
+            value={politicalPref}
+            onChange={(e) => setPoliticalPref(e.target.value)}
+          >
             <option value="Liberal">Liberal</option>
             <option value="Conservative">Conservative</option>
           </select>
         </div>
 
         <div className="slider">
-          <label className="form-text" >
-            Age
-          </label>
+          <label className="form-text">Age</label>
           <input id="range" type="range" value="number" />
         </div>
 
         <div className="checkbox-div">
-          <label id="exercise" className="form-text" >
+          <label id="exercise" className="form-text">
             Smoker?
           </label>
-          <input className="checkbox" type="checkbox" value={smokerPref} onChange={(e)=> setSmokerPref(e.target.checked)}/>
-          <label id="meditation" className="form-text" >
+          <input
+            className="checkbox"
+            type="checkbox"
+            value={smokerPref}
+            onChange={(e) => setSmokerPref(e.target.checked)}
+          />
+          <label id="meditation" className="form-text">
             Drinker?
           </label>
-          <input className="checkbox" type="checkbox" value={drinkerPref} onChange={(e)=> setDrinkerPref(e.target.checked)}/>
+          <input
+            className="checkbox"
+            type="checkbox"
+            value={drinkerPref}
+            onChange={(e) => setDrinkerPref(e.target.checked)}
+          />
         </div>
         <button id="form-submit">Submit</button>
       </form>
