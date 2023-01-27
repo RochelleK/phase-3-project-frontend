d4b9604f473ec544614a66454a0412774ef4817d
Olivia Radick <olivia.radick@gmail.com>
2023-01-27 12:15:10 -0500

    back button for carousel

--- src/components/MatchCarousel.jsx
+++ src/components/MatchCarousel.jsx
@@ -2,19 +2,29 @@ import React from "react";
 import Match from "./Match";
 
 function MatchCarousel({ matches, setBeltPosition, beltPosition, startChat, updateMatches, setMatches}) {
-  function addMoreMatches() {
+  
+  function showPreviousMatches() {
+    setBeltPosition(beltPosition - 1);
+    console.log("back button clicked");
+  }
+  function showNextMatches() {
     setBeltPosition(beltPosition + 1);
-    console.log("button clicked");
+    console.log("next button clicked");
   }
   return (
     <div className="matchcaraousel">
+      <img
+        src="assets/back-button.png"
+        className="match-button"
+        onClick={showPreviousMatches}
+      />
       {matches.map((match) => (
         <Match key={match.id} match={match} startChat={startChat} updateMatches={updateMatches} setMatches={setMatches}/>
       ))}
       <img
         src="/assets/next2.png"
         className="match-button"
-        onClick={addMoreMatches}
+        onClick={showNextMatches}
       />
     </div>
   );
